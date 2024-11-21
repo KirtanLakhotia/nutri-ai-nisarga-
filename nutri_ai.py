f9263d583230c9df5b24fb9f15d74124848392b2
KirtanLakhotia <122653509+KirtanLakhotia@users.noreply.github.com>
2024-11-21 17:47:18 +0530

    Update nutri_ai.py

--- nutri_ai.py
+++ nutri_ai.py
@@ -21,4 +21,4 @@ def hello_print():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=4000)
+    uvicorn.run(app, host="0.0.0.0", port=4000||process.env.PORT)
